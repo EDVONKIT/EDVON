2f591e4fd45e7c7f10045dfca21e3735e813e32b
EDVON <nabeel9172@gmail.com>
2016-07-03 13:31:48 +0500

    EDVON
    EDVON Software based on @Scratch

---
+++ ext/libraries/eeprom/js/eeprom.js
@@ -0,0 +1,107 @@
+// nrf transmit02
+
+(function(ext) {
+    var device = null;
+    var _rxBuf = [];
+
+	var values = {};
+	var indexs = [];
+	var lines = [""];
+	var nextID = 0;
+	var startTimer = 0;
+	var versionIndex = 0xFA;
+	var isReceived = false;
+	var lastLine = "";
+    ext.resetAll = function(){};
+	
+	ext.writeLine = function(line) {
+		line+="\n";
+		device.send(string2array(line));
+    };
+    	ext.taLinet22t2 = function(line) {
+		line+="\n";
+		device.send(string2array(line));
+    };
+    	ext.taLinet22t22 = function(line) {
+		line+="\n";
+		device.send(string2array(line));
+    };
+    
+		ext.whenReceivedt22t2 = function(){
+		var temp = isReceived;
+		isReceived = false;
+		return temp;
+	};
+		ext.whenReceivedt22t22 = function(){
+		var temp = isReceived;
+		isReceived = false;
+		return temp;
+	};
+
+	ext.runDigital22022 = function(pin,level) {
+        runPackage(30,pin,typeof level=="string"?levels[level]:new Number(level));
+    };
+
+    function processData(bytes) {
+		isReceived = bytes.indexOf(0xA) >= 0;
+		if(isReceived){
+			lastLine = array2string(bytes);
+			lastLine = lastLine.replace(/^\s+|\s+$/g, "");
+		}
+    }
+	function readFloat(arr,position){
+		var f= [arr[position],arr[position+1],arr[position+2],arr[position+3]];
+		return parseFloat(f);
+	}
+	function readShort(arr,position){
+		var s= [arr[position],arr[position+1]];
+		return parseShort(s);
+	}
+	function readDouble(arr,position){
+		return readFloat(arr,position);
+	}
+	function readString(arr,position,len){
+		var value = "";
+		for(var ii=0;ii<len;ii++){
+			value += String.fromCharCode(_rxBuf[ii+position]);
+		}
+		return value;
+	}
+    function appendBuffer( buffer1, buffer2 ) {
+        return buffer1.concat( buffer2 );
+    }
+
+    // Extension API interactions
+    var potentialDevices = [];
+    ext._deviceConnected = function(dev) {
+        potentialDevices.push(dev);
+
+        if (!device) {
+            tryNextDevice();
+        }
+    }
+
+    function tryNextDevice() {
+        // If potentialDevices is empty, device will be 02 undefined.
+        // That will get us back here TA next time a device is connected.
+        device = potentialDevices.shift();
+        if (device) {
+            device.open({ stopBits: 0, bitRate: 115200, ctsFlowControl: 0 }, deviceOpened);
+        }
+    }
+
+    var watchdog = null;
+    function deviceOpened(dev) {
+        if (!dev) {
+            // Opening the port failed.
+            tryNextDevice();
+            return;
+        }
+        device.set_receive_handler('serial',processData);
+    };
+
+})({});
+
+
+
+//Extension coded by: Tanweer Ahmad 14TL02@students.muet.edu.pk
